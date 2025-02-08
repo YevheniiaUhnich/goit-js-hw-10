297e264ee15402bef2b411f48e2f086f9c32030d
YevheniiaUhnich <140949326+YevheniiaUhnich@users.noreply.github.com>
2025-02-08 22:34:55 +0200

    update

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,26 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css"; 
+
+// const form = document.querySelector(.form);
+// form.addEventListener('submit', onSubmit);
+
+// function onSubmit(e) {
+//     e.preventDefault();
+// const {delay.value, state.value} = e.currentTarget.elements;
+// creatPromise(delay, state.value).then((data) => iziToast.success({message: `✅ Fulfilled promise in ${delay}ms`} 
+//     position: topRight)).catch(data) => iziToast.error({message: `❌ Rejected promise in ${delay}ms`} position: topRight)
+
+// form.reset();
+
+// function createPromise(delay, state) {
+//     return new Promise((resolve, reject) =>{
+//         setTimeout(() => {
+//             if(state ==="fulfilled"){resolve ("delay")
+//             }else{
+//         reject(delay);
+//     }
+//     } 
+//     delay);
+//     })
+// }
+// }
