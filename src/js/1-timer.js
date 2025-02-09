05aed098c69c8d23173806ae250ac4c4a136d441
YevheniiaUhnich <140949326+YevheniiaUhnich@users.noreply.github.com>
2025-02-09 20:29:20 +0200

    update

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,3 +1,73 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const dateInput = document.getElementById('datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+let timerId = null;
+let selectedDate = null;
+startBtn.addEventListener('click', startTimer);
+
+flatpickr(dateInput, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] <= new Date()) {
+      iziToast.info({
+        position: 'topRight',
+        message: 'Please choose a date in the future',
+      });
+    } else {
+      selectedDate = selectedDates[0];
+      startBtn.removeAttribute('disabled');
+    }
+  },
+});
+
+function startTimer() {
+  timerId = setInterval(updateTimer, 1000);
+  startBtn.setAttribute('disabled', true);
+  dateInput.setAttribute('disabled', true);
+}
+function updateTimer() {
+  const diff = selectedDate - new Date();
+  const { days, hours, minutes, seconds } = convertMs(diff);
+  daysEl.textContent = addLeadingZero(days);
+  hoursEl.textContent = addLeadingZero(hours);
+  minutesEl.textContent = addLeadingZero(minutes);
+  secondsEl.textContent = addLeadingZero(seconds);
+  if (diff <= 1000) {
+    clearInterval(timerId);
+    dateInput.removeAttribute('disabled');
+  }
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
